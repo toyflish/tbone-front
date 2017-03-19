a79e0cb23e543400ffefe3686b23b939930d7910
Kai Rautenberg <mail@kairautenberg.de>
2017-03-19 22:49:28 +0100

    archive styles

--- src/components/Archive.vue
+++ src/components/Archive.vue
@@ -4,16 +4,15 @@
     <img v-bind:src="node.attachment_url" />
     <div v-html="node.content"></div>
     <div class="children">
-      <div class="child" v-for="child in node.children">
-        <router-link :to="{ path: child.slug }" style="display:block">
-          <img v-if="child.attachment_url" v-bind:src="child.attachment_url" v-bind:alt="child.name"/>
-          <span v-else class="primer">{{child.name}}</span>
-        </router-link>
-        <div class="grand-children">
-          <div class="grand-child" v-for="grandChild in child.children.slice(0,4)">
-            <router-link :to="{ path: grandChild.slug }" style="display:block">
-              <img v-if="grandChild.preview_url" v-bind:src="grandChild.preview_url" v-bind:alt="grandChild.name"/>
+      <div class="square" v-for="child in node.children">
+        <div class="content" v-on:click="$router.push({ path: child.slug })">
+          <div class="name">
+            <router-link :to="{ path: child.slug }" style="display:block">
+              <span>{{child.name}}</span>
             </router-link>
+          </div>
+          <div class="descendants">
+            <span>{{child.descendant_count}}</span>
           </div>
         </div>
       </div>
@@ -24,7 +23,9 @@
 <script>
   export default {
     name: 'archive',
-    props: ['node']
+    props: ['node'],
+    methods: {
+    }
   }
 </script>
 
@@ -32,20 +33,48 @@
 <style lang="scss">
 .archive {
   .children {
-    .child {
-      .grand-children {
-        margin-bottom: 3px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    .square {
+      border: 1px solid #fff;
+      position: relative;
+      height: 0;
+      width: 33.33%;
+      padding-bottom: 33.33%;
+      .content {
+        cursor: pointer;
+        background: #000;
+        position:  absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+
         display: flex;
-        flex-wrap: wrap;
-        .grand-child {
-          width: 24.7%;
-          margin-right: 0.4%;
-          * {
-            display: block;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        .name {
+          width: 70%;
+          font-weight: bold;
+          font-size:1.2em;
+          a {
+            color: #fff;
           }
+
         }
-        .grand-child:nth-child(4) {
-          margin-right: 0;
+        .descendants {
+          margin:5px;
+          color: #aaa;
+          font-size: smaller;
+          font-weight: 400;
+        }
+        &:hover {
+          background: #fff;
+          .name a{
+            color: #000;
+          }
         }
       }
     }
