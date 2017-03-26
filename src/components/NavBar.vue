4684bed47fb544334a63566455132119ef942f00
Kai Rautenberg <mail@kairautenberg.de>
2017-03-26 17:11:34 +0100

    +event based main menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <div class="nav">
     <div class="container">
       <div class="hamburger-wrapper">
-        <div v-on:click="toggleMenuState" class="hamburger-icon" v-bind:class="{ open: menuOpen }">
+        <div v-on:click="hamburgerClick" class="hamburger-icon" v-bind:class="{ open: menuOpen }">
           <span></span>
           <span></span>
           <span></span>
@@ -10,7 +10,7 @@
       </div>
       <div class="breadcrumb">
         <div v-for="crumb in breadcrumb">
-          <router-link :to="{path: sanitizeSlug(crumb.href)}">
+          <router-link :to="{path: crumb.href}">
             <BreadCrumbArrow />
           </router-link>
         </div>
@@ -19,7 +19,7 @@
         <nav>
           <ul>
             <li style="height: 20%" v-for="item in items">
-              <router-link :to="{path: sanitizeSlug(item.href)}" v-on:click.native="toggleMenuState">
+              <router-link :to="{path: item.href}" v-on:click.native="$emit('closeMainMenu')">
                 {{item.link_name}}
               </router-link>
             </li>
@@ -44,7 +44,6 @@ export default {
   },
   computed: {
     breadcrumb: function () {
-      console.log('NavBar::computed:breadcrumb')
       if (this.$route.path === '/') {
         return []
       } else {
@@ -63,20 +62,22 @@ export default {
       parents.unshift(node)
       return parents
     },
-
-    historyBack: function () {
-      console.log('history back')
-    },
-    toggleMenuState: function () {
-      this.menuOpen = !this.menuOpen
-      console.log('toggleMenuState')
-    },
-    // sanitize root slug from "" to "/"
-    sanitizeSlug: function (slug) {
-      return slug === '' ? '/' : slug
+    hamburgerClick: function () {
+      console.log('emiting: ', this.$store.state.hamburgerClickEvent)
+      this.$emit(this.$store.state.hamburgerClickEvent)
     }
   },
   mounted: function () {
+    this.$on('openMainMenu', function () {
+      this.menuOpen = true
+      this.$store.commit('setHamburgerClickEvent', 'closeMainMenu')
+    })
+
+    this.$on('closeMainMenu', function () {
+      this.menuOpen = false
+      this.$store.commit('setHamburgerClickEvent', 'openMainMenu')
+    })
+
     let thisView = this
     axios.get(`${this.$store.state.apiUrl}/api/nodes/menu.json`)
     .then(function (response) {
--- src/main.js
+++ src/main.js
@@ -8,8 +8,6 @@ import './css/denormalize.scss'
 import Vue from 'vue'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
-import Goals from './components/Goals'
-import Achievements from './components/Achievements'
 import NodeDispatcher from './components/NodeDispatcher'
 
 import VueRouter from 'vue-router'
@@ -18,7 +16,6 @@ import Vuex from 'vuex'
 import VueAnalytics from 'vue-analytics'
 
 const apiUrl = process.env.API_URL
-console.log('api url', apiUrl)
 
 Vue.use(Vuex)
 Vue.use(VueRouter)
@@ -27,12 +24,17 @@ const store = new Vuex.Store({
   strict: true,
   state: {
     activeNode: {name: 'initial'},
-    apiUrl: apiUrl
+    apiUrl: apiUrl,
+    overlayOpen: false,
+    hamburgerClickEvent: 'openMainMenu'
   },
   mutations: {
     setActiveNode (state, node) {
       console.log('Store:mutations:setActiveNode', node)
       state.activeNode = node
+    },
+    setHamburgerClickEvent: function (state, ename) {
+      state.hamburgerClickEvent = ename
     }
   }
 })
@@ -41,11 +43,9 @@ const store = new Vuex.Store({
 let setDocumentTitle = function (title) {
   document.title = title
 }
-
 let setDocumentTitleByActiveNode = function () {
   setDocumentTitle(store.state.activeNode.title)
 }
-
 store.watch(
   function (state) {
     return state.activeNode
@@ -60,16 +60,13 @@ const router = new VueRouter({
   mode: 'history',
   routes: [
     {
-      path: '/goals',
-      component: Goals
-    },
-    {
-      path: '/achievements',
-      component: Achievements
-    },
-    {
       path: '/',
       name: 'root',
+      component: NodeDispatcher
+    },
+    {
+      path: '/:slug/:highlight',
+      name: 'nodeSlugHighlight',
       component: NodeDispatcher
     },
     {
@@ -78,6 +75,16 @@ const router = new VueRouter({
       component: NodeDispatcher
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  console.log('ROUTE DEBUG to:', to, 'from:', from)
+  if (to.params.slug === from.params.slug) {
+    // next(false)
+    next()
+  } else {
+    next()
+  }
 })
 
 // activate analytics only on production
